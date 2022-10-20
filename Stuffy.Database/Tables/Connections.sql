8044fcb7c627d21941684f3dc6375fc7632e7fea
Lilith <evilgamerx42@gmail.com>
2022-10-20 14:24:52 -0400

    Deleting the old files that are no longer use

--- Stuffy.Database/Tables/Connections.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[Connections]
-(
-	[Id] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY, 
-    [Relationship] NVARCHAR(MAX) NOT NULL, 
-    [ColourCode] INT NOT NULL, 
-    [ParentId] UNIQUEIDENTIFIER NOT NULL, 
-    [NodeId] UNIQUEIDENTIFIER NOT NULL, 
-    CONSTRAINT [FK_Connections_OtherNodes] FOREIGN KEY ([NodeId]) REFERENCES [Nodes]([Id])
-)
